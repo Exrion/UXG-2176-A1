03efa07d5636977785de92c57c4e5c2539380695
Cai Xuan <exrion.lexom@gmail.com>
2026-09-21 10:00:26 +0800

    basic bot done

--- UXG2176_A1/Assets/A1/A1Bot/BitBotBT.cs
+++ UXG2176_A1/Assets/A1/A1Bot/BitBotBT.cs
@@ -1,7 +1,10 @@
 using Assignment1;
 using BehaviourTree;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 namespace BitBotBehaviourTree
 {
@@ -12,11 +15,13 @@ namespace BitBotBehaviourTree
         {
             Node root = new Selector(new List<Node>
             {
+                new Reload(),
                 new Dodge(),
                 new Selector(new List<Node>
                 {
                     new ShootFar(),
-                    new ShootNear()
+                    new ShootNear(),
+                    new Melee()
                 }),
                 new Selector(new List<Node>
                 {
@@ -30,31 +35,107 @@ namespace BitBotBehaviourTree
 
     public class Dodge : Node
     {
+        private const int AngleSteps = 16;
+        private const int HorizonSteps = 16;
+        private List<Vector3> currentPositions = new();
+        private List<Vector3> previousPositions = new();
+
         public override NodeState Evaluate()
         {
             var controller = (AutoBotController)parent.GetData("controller");
             var projectiles = controller.GetEnemyProjectilePosition();
+            var playerPos = controller.GetAutoBotPosition();
 
-            return base.Evaluate();
+            if (projectiles.Count == 0)
+            {
+                return NodeState.Failure;
+            }   
+
+            if (projectiles.Count != currentPositions.Count)
+            {
+                currentPositions = new List<Vector3>(projectiles);
+                previousPositions = new List<Vector3>(currentPositions);
+                return NodeState.Failure;
+            }
+            else
+            {
+                previousPositions = new List<Vector3>(currentPositions);
+                currentPositions = new List<Vector3>(projectiles);
+            }
+
+            var moveDir = GetDodgeDir();
+            if (moveDir == Vector3.zero)
+            {
+                return NodeState.Failure;
+            }
+            controller.SetMoveDir(moveDir);
+            return NodeState.Running;
+        }
+
+        private Vector3 GetDodgeDir()
+        {
+            Vector3 bestDir = Vector3.zero;
+            float bestScore = float.NegativeInfinity;
+
+            for (int i = 0; i < AngleSteps; i++)
+            {
+                float angle = (float)(i * 2.0 * Math.PI / AngleSteps);
+                var dir = new Vector3((float)Math.Cos(angle), (float)Math.Sin(angle));
+
+                float score = EvalDir(dir);
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestDir = dir;
+                }
+            }
+
+            return bestDir;
+        }
+
+        private float EvalDir(Vector3 dir)
+        {
+            float worstClearance = float.MaxValue;
+            var controller = (AutoBotController)parent.GetData("controller");
+
+            int count = 0;
+            foreach (var p in currentPositions)
+            {
+                for (int t = 0; t <= HorizonSteps; t++)
+                {
+                    Vector3 botFuture = controller.GetAutoBotPosition() + dir * 5f * Time.deltaTime;
+                    Vector3 projFuture = ((p - previousPositions[count]).normalized * 7f * Time.deltaTime) + p;
+
+                    float dist = Vector2.Distance(botFuture, projFuture);
+                    float clearance = dist - (1f + .5f * .3f);
+                    float timeWeight = 1f / (1f + t * 0.15f);
+                    float weighted = clearance * timeWeight;
+
+                    if (weighted < worstClearance)
+                        worstClearance = weighted;
+                }
+                count++;
+            }
+
+            return currentPositions.Count == 0 ? 0f : worstClearance;
         }
     }
 
     public class Chase : Node
     {
+        private const float chaseDistanceScalar = .9f;
+
         public override NodeState Evaluate()
         {
             var controller = (AutoBotController)parent.GetData("controller");
             float currentDistance = Vector3.Distance(controller.GetAutoBotPosition(), controller.GetEnemyPosition());
-            if (currentDistance < controller.GetProjectileMaxDistance() * .75f)
+            if (currentDistance < controller.GetProjectileMaxDistance() * chaseDistanceScalar)
             {
+                controller.SetMoveDir(Vector3.zero);
                 return NodeState.Failure;
             }
             else
             {
-                if (controller.GetProjectileLeft() == 0)
-                {
-                    return NodeState.Failure;
-                }
                 var enemyPos = controller.GetEnemyPosition();
                 var playerPos = controller.GetAutoBotPosition();
                 var diff = enemyPos - playerPos;
@@ -66,25 +147,136 @@ namespace BitBotBehaviourTree
 
     public class Retreat : Node
     {
+        private const float retreatDistanceScalar = 1.2f;
+        private const float retreatQuadrantThreshold = 0.5f;
+        private readonly Vector2 mapHalfSize = new(8f, 4f);
+
         public override NodeState Evaluate()
         {
-            return base.Evaluate();
+            var controller = (AutoBotController)parent.GetData("controller");
+            float currentDistance = Vector3.Distance(controller.GetAutoBotPosition(), controller.GetEnemyPosition());
+            if (currentDistance > controller.GetProjectileMaxDistance() * retreatDistanceScalar)
+            {
+                controller.SetMoveDir(Vector3.zero);
+                return NodeState.Failure;
+            }
+            else
+            {
+                var enemyPos = controller.GetEnemyPosition();
+                var playerPos = controller.GetAutoBotPosition();
+                var diff = enemyPos - playerPos;
+
+                // Bot Left
+                if (playerPos.x < -(mapHalfSize.x * retreatQuadrantThreshold) && playerPos.y < -(mapHalfSize.y * retreatQuadrantThreshold))
+                {
+                    diff.x += diff.x * playerPos.x / -mapHalfSize.x + playerPos.x / -mapHalfSize.x;
+                    diff.y += diff.y * playerPos.y / -mapHalfSize.y + playerPos.y / -mapHalfSize.y;
+                }
+                // Bot Right
+                else if (playerPos.x < (mapHalfSize.x * retreatQuadrantThreshold) && playerPos.y < -(mapHalfSize.y * retreatQuadrantThreshold))
+                {
+                    diff.x += diff.x * playerPos.x / mapHalfSize.x + playerPos.x / mapHalfSize.x;
+                    diff.y += diff.y * playerPos.y / -mapHalfSize.y + playerPos.y / -mapHalfSize.y;
+                }
+                // Top Left
+                else if (playerPos.x < -(mapHalfSize.x * retreatQuadrantThreshold) && playerPos.y < (mapHalfSize.y * retreatQuadrantThreshold))
+                {
+                    diff.x += diff.x * playerPos.x / -mapHalfSize.x + playerPos.x / -mapHalfSize.x;
+                    diff.y += diff.y * playerPos.y / mapHalfSize.y + playerPos.y / mapHalfSize.y;
+                }
+                // Top Right
+                else if (playerPos.x < (mapHalfSize.x * retreatQuadrantThreshold) && playerPos.y < (mapHalfSize.y * retreatQuadrantThreshold))
+                {
+                    diff.x += diff.x * playerPos.x / mapHalfSize.x + playerPos.x / mapHalfSize.x;
+                    diff.y += diff.y * playerPos.y / mapHalfSize.y + playerPos.y / mapHalfSize.y;
+                }
+                controller.SetMoveDir(-diff);
+            }
+            Debug.Log("CX Bot:\t Retreat");
+            return NodeState.Success;
         }
     }
 
     public class ShootFar : Node
     {
+        private const float shootDistanceScalar = 0.8f;
+
         public override NodeState Evaluate()
         {
-            return base.Evaluate();
+            var controller = (AutoBotController)parent.GetData("controller");
+            var projLeft = controller.GetProjectileLeft();
+            float currentDistance = Vector3.Distance(controller.GetAutoBotPosition(), controller.GetEnemyPosition());
+
+            if (projLeft > 0 && currentDistance > controller.GetProjectileMaxDistance() * shootDistanceScalar && currentDistance <= controller.GetProjectileMaxDistance())
+            {
+                var enemyPos = controller.GetEnemyPosition();
+                var playerPos = controller.GetAutoBotPosition();
+                var diff = enemyPos - playerPos;
+                controller.SetFacing(diff);
+                if (projLeft >= 2)
+                {
+                    controller.Fire(diff);
+                    return NodeState.Success;
+                }
+                return NodeState.Failure;
+            }
+            return NodeState.Failure;
         }
     }
 
     public class ShootNear : Node
     {
+        private const float shootDistanceScalar = 0.4f;
+
         public override NodeState Evaluate()
         {
-            return base.Evaluate();
+            var controller = (AutoBotController)parent.GetData("controller");
+            var projLeft = controller.GetProjectileLeft();
+            float currentDistance = Vector3.Distance(controller.GetAutoBotPosition(), controller.GetEnemyPosition());
+
+            if (projLeft > 0 && currentDistance > controller.GetProjectileMaxDistance() * shootDistanceScalar && currentDistance <= controller.GetProjectileMaxDistance())
+            {
+                var enemyPos = controller.GetEnemyPosition();
+                var playerPos = controller.GetAutoBotPosition();
+                var diff = enemyPos - playerPos;
+                controller.SetFacing(diff);
+                if (projLeft >= 3)
+                {
+                    controller.Fire(diff);
+                    controller.Fire(diff);
+                    return NodeState.Success;
+                }
+                else
+                {
+                    if (projLeft == 1)
+                        return NodeState.Failure;
+                    controller.Fire(diff);
+                    return NodeState.Success;
+                }
+            }
+            return NodeState.Failure;
+        }
+    }
+
+    public class Melee : Node
+    {
+        public override NodeState Evaluate()
+        {
+            return NodeState.Failure;
+        }
+    }
+
+    public class Reload : Node
+    {
+        public override NodeState Evaluate()
+        {
+            var controller = (AutoBotController)parent.GetData("controller");
+            if (!controller.IsReloading() && controller.GetProjectileLeft() < 5)
+            {
+                controller.Reload();
+                return NodeState.Success;
+            }
+            return NodeState.Failure;
         }
     }
 }
